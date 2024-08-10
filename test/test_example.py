f64b9f7eb7b805032d64bc18060dbe97b33977fe
Andi Wilson <andialexwilson@gmail.com>
2024-08-10 11:44:14 -0700

    coding challenge fetch

---
+++ test/test_example.py
@@ -0,0 +1,20 @@
+import re
+from playwright.sync_api import Page, expect
+
+# def test_has_title(page: Page):
+#     page.goto("http://sdetchallenge.fetch.com/")
+
+#     # Expect a title "to contain" a substring.
+#     expect(page).to_have_title(re.compile("React App"))
+
+# def test_has_rest_button(page: Page):
+#     page.goto("http://sdetchallenge.fetch.com/")
+#     expect(page.get_by_role("button", name="Reset")).to_be_visible()
+
+# def test_has_weigh_button(page: Page):
+#     page.goto("http://sdetchallenge.fetch.com/")
+#     expect(page.get_by_role("button", name="Weigh")).to_be_visible()
+
+
+
+
